e87e8114ff10a921db1e8ccac53d3954a6851dc8
Dhathri33 <dbathini@luc.edu>
2025-11-12 21:41:56 -0600

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Dhathri Bathini on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
